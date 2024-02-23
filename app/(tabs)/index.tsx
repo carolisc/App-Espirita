1192d1b13430eddc9ea02d2ea7d9c6418edabc7a
Carolis <89231697+carolisc@users.noreply.github.com>
2024-02-22 22:28:12 -0300

    install expo router with tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+import React, { useState } from 'react';
+import { Button } from 'react-native';
+
+const phrases = [
+  '"A nossa felicidade será naturalmente proporcional em relação à felicidade que fizermos para os outros." ',
+  '"Fé inabalável só o é a que pode encarar frente a frente a razão, em todas as épocas da Humanidade." ',
+  '"O corpo existe tão somente para que o Espírito se manifeste." ',
+  // Add more phrases as needed
+];
+
+// TO START THE APP: npm start
+
+// TODO 1: fazer página com várias opções de meditação e deixar disponíveis, preces tbm
+
+// TODO 2: ver como fazer pra mandar os estilos pra outra página
+
+const App = () => {
+  const [randomPhrase, setRandomPhrase] = useState('');
+
+  const getRandomPhrase = () => {
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    setRandomPhrase(phrases[randomIndex]);
+  };
+
+  return (
+      <View style={{ 
+          flex: 1, 
+          // justifyContent: 'space-around', 
+          alignItems: 'center',
+        }}>
+
+        <View>
+          <Text style={{
+            fontSize: 40,
+            padding: 20,
+            marginTop: 100,
+          }}>Bem vindo(a) ao AppEspirita!</Text>
+          <Text style={{
+            fontSize: 25,
+            padding: 20,
+          }} >Veja uma frase de Kardec clicando no botão:</Text>
+        </View>
+
+        <View style={{
+          marginTop: 40,
+          padding: 30,
+        }}>
+          <Button title="Ler Frase do Dia" onPress={getRandomPhrase} />
+          <Text style={{
+            marginTop: 40,
+            padding: 30,
+            fontSize: 25,
+          }}>{randomPhrase} - Allan Kardec</Text>
+        </View>
+      </View>
+  );
+};
+
+export default App;
